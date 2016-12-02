c0c8040268931587596cb69d65da901329b28499
Sebastian Thoss <sebastian.thoss@kartenmacherei.de>
2016-12-02 08:59:22 +0100

    Initial Commit of code

--- smoke-testing.js
+++ smoke-testing.js
@@ -1,12 +1,13 @@
 var http = require('http');
 
-var paths = [
-    '/de/', //working
-    '/en/', //working
-    '/foo-bar' //not working
-];
+var LineByLineReader = require('line-by-line');
+var lr = new LineByLineReader('paths.txt');
 
-paths.forEach(function (path) {
+lr.on('error', function (error) {
+    console.log(error);
+});
+
+lr.on('line', function (path) {
     var options = {
         hostname: 'localhost',
         port: 4000,
@@ -14,18 +15,23 @@ paths.forEach(function (path) {
         method: 'GET'
     };
 
-    http.get(options, function(response){
-        console.log(response.req.path + ' - ' + response.statusCode);
+    http.get(options, function (response) {
+        if (response.statusCode != 200) {
+            console.error(
+                'http://' + options.hostname + ':' + options.port + response.req.path +
+                ' - ' + response.statusCode
+            );
+        }
 
         var bodyChunks = [];
         response
-        .on('data', function(chunk) {
-            bodyChunks.push(chunk);
-        })
-        .on('end', function() {
-            var body = Buffer.concat(bodyChunks);
-            //TODO do stuff with body
-            //console.log(response.req.path);
-        });
+            .on('data', function (chunk) {
+                bodyChunks.push(chunk);
+            })
+            .on('end', function () {
+                var body = Buffer.concat(bodyChunks);
+                //TODO do stuff with body
+                //console.log(response.req.path);
+            });
     });
 });
